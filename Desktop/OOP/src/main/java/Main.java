7bb004f5b342131cd7fd07cced3a107eb73eaacb
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2022-11-26 16:28:29 +0300

    c

---
+++ Desktop/OOP/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import chars.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static List<Base> whiteSide;
+    public static List<Base> darkSide;
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
